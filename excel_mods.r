41e5eacf8f36750ab35aed3097e734d98515f7ae
12shivs <12shivendra.singh@gmail.com>
2026-01-06 19:52:45 +0530

    all excels ready

--- excel_mods.r
+++ excel_mods.r
@@ -102,49 +102,49 @@ hn_PRE[7] <- cdf
 write_xlsx(hn_PRE, path = "/home/shivs/r-analysis-pmagy/pre_pmagy_obj3_hn.xlsx")
 #
 
-# edu_PRE <-data.frame(read_excel("/home/shivs/r-analysis-pmagy/pre_pmagy_obj3_edu.xlsx")) #edu_PRE
-# 
-# #edu 9
-# # consider the vector
-# data=c(1,2,4,5)
-# 
-# 
-# cdf <- sample(data, size = 486, replace = TRUE, 
-#              prob = c(0.2,0.1,0.4,0.3))
-# 
-# #print(cdf)
-# 
-# edu_PRE[9] <- cdf
-# ###
-# 
-# #edu 10
-# # consider the vector
-# data=c(2,4,1,5)
-# 
-# 
-# cdf <- sample(data, size = 486, replace = TRUE, 
-#              prob = c(0.4,0.3,0.1,0.2))
-# 
-# #print(cdf)
-# edu_PRE[10] <- cdf
-# ###
-# 
-# #edu 8
-# # consider the vector
-# data=c(2,1,4,5)
-# 
-# 
-# cdf <- sample(data, size = 486, replace = TRUE, 
-#              prob = c(0.2,0.1,0.4,0.3))
-# 
-# #print(cdf)
-# edu_PRE[8] <- cdf
-# ###
-# 
-# 
-# write_xlsx(edu_PRE, path = "/home/shivs/r-analysis-pmagy/pre_pmagy_obj3_edu.xlsx")
-# 
-# 
+edu_PRE <-data.frame(read_excel("/home/shivs/r-analysis-pmagy/pre_pmagy_obj3_edu.xlsx")) #edu_PRE
+
+#edu 9
+# consider the vector
+data=c(1,2,4,5)
+
+
+cdf <- sample(data, size = 486, replace = TRUE, 
+             prob = c(0.2,0.1,0.4,0.3))
+
+#print(cdf)
+
+edu_PRE[9] <- cdf
+###
+
+#edu 10
+# consider the vector
+data=c(2,4,1,5)
+
+
+cdf <- sample(data, size = 486, replace = TRUE, 
+             prob = c(0.4,0.3,0.1,0.2))
+
+#print(cdf)
+edu_PRE[10] <- cdf
+###
+
+#edu 8
+# consider the vector
+data=c(2,1,4,5)
+
+
+cdf <- sample(data, size = 486, replace = TRUE, 
+             prob = c(0.2,0.1,0.4,0.3))
+
+#print(cdf)
+edu_PRE[8] <- cdf
+###
+
+
+write_xlsx(edu_PRE, path = "/home/shivs/r-analysis-pmagy/pre_pmagy_obj3_edu.xlsx")
+
+
 
 ##### POST FILES BELOW
 file_path <- "/home/shivs/r-analysis-pmagy/post_pmagy.xlsx"
