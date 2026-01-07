56195bbd21462158636a22766407da78a83a373c
12shivs <12shivendra.singh@gmail.com>
2026-01-07 14:29:23 +0530

    obj4 all TEST DOONE

---
+++ obj4_all_tests.r
@@ -0,0 +1,80 @@
+# obj 3. To analyze the impact of PMAGY on education and health facilities in the selected villages.
+
+# import packages
+library(readxl)
+library(ltm)
+library(dplyr)
+# Load the purrr library for iteration
+library(purrr)
+
+post_pmagy_file_path <- "/home/shivs/r-analysis-pmagy/post_pmagy.xlsx"
+
+### lisd Section tests:
+
+print("lisd Section tests: ")
+#POST PMAGY lisd
+lisd_POST <-data.frame(read_excel(post_pmagy_file_path,sheet = "Sheet1",range = "CJ1:CN487")) #lisd_POST
+colnames(lisd_POST) <- paste('POST', colnames(lisd_POST), sep = '_')
+#PRE PMAGY lisd
+lisd_PRE <-data.frame(read_excel("/home/shivs/r-analysis-pmagy/pre_pmagy_obj4_lisd.xlsx",range = "A1:E487")) #lisd_PRE
+colnames(lisd_PRE) <- paste('PRE', colnames(lisd_PRE), sep = '_')
+
+
+#calculate Cronbach's Alpha:
+print(cronbach.alpha(lisd_POST))
+print(cronbach.alpha(lisd_PRE))
+#
+
+#Variance Inflation Factor (VIF):
+library(car)
+library(caTools)
+# Fit a regression model on post data
+lisd_POST_w_index <-data.frame(read_excel(post_pmagy_file_path,sheet = "Sheet1",range = "CJ1:CO487"))
+colnames(lisd_POST_w_index) <- paste('POST', colnames(lisd_POST_w_index), sep = '_')
+model_all <- lm(POST_LISD_INDEX ~ ., data = lisd_POST_w_index)
+# Calculate VIF
+vif_results <- car::vif(model_all)
+
+print("VIF results for post data:")
+print(vif_results)
+
+# Fit a regression model on pre data
+lisd_PRE_w_index <-data.frame(read_excel("/home/shivs/r-analysis-pmagy/pre_pmagy_obj4_lisd.xlsx",range = "A1:F487"))
+colnames(lisd_PRE_w_index) <- paste('PRE', colnames(lisd_PRE_w_index), sep = '_')
+model_all <- lm(PRE_LISD_INDEX ~ ., data = lisd_PRE_w_index)
+# Calculate VIF
+vif_results <- car::vif(model_all)
+print("VIF results for pre data:")
+print(vif_results)
+#TODO: Visualizing VIF Values from here https://www.geeksforgeeks.org/r-language/vif-function-in-r/
+#
+
+# Using theShapiro–Wilk Test :
+print("Shapiro–Wilk Test results for pre data:")
+print(shapiro.test(lisd_PRE_w_index$PRE_LISD_INDEX))
+print("Shapiro–Wilk Test results for post data:")
+print(shapiro.test(lisd_POST_w_index$POST_LISD_INDEX))
+#
+
+# Paired Samples Wilcoxon Test : 
+
+# combine two data frames horizontally
+lisd_pre_post_hcomb <- cbind(lisd_PRE, lisd_POST)
+
+# # Define a function to perform paired Paired Samples Wilcoxon Test between pairs of columns
+paired_wilcoxon_test <- function(before, after) {
+  wilcox.test(before, after, paired = TRUE)
+}
+
+# # Apply paired Paired Samples Wilcoxon Test to each pair of pre and post columns
+results_wilcoxon_test <- map2(lisd_pre_post_hcomb[grepl("PRE", names(lisd_pre_post_hcomb))],
+                lisd_pre_post_hcomb[grepl("POST", names(lisd_pre_post_hcomb))],
+                paired_wilcoxon_test)
+
+# # Print the results
+print("Print the results for column/question wise Paired Samples Wilcoxon Test")
+print(results_wilcoxon_test)
+#
+
+### lisd Section tests COMPLETE
+print("lisd Section tests COMPLETE")
