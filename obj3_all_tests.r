8d8ed592cb12af6597c07649a8d19c58ba5198ab
12shivs <12shivendra.singh@gmail.com>
2026-01-07 11:35:11 +0530

    vif tests done

--- obj3_all_tests.r
+++ obj3_all_tests.r
@@ -24,15 +24,28 @@ print(cronbach.alpha(edu_PRE))
 
 #Variance Inflation Factor (VIF):
 library(car)
-
-# Fit a regression model
+library(caTools)
+# Fit a regression model on post data
 edu_POST_w_index <-data.frame(read_excel(file_path,sheet = "Sheet1",range = "AE1:AO487"))
-model <- lm(EDU_INDEX ~ ., data = edu_POST_w_index)
+colnames(edu_POST_w_index) <- paste('POST', colnames(edu_POST_w_index), sep = '_')
+model_all <- lm(POST_EDU_INDEX ~ ., data = edu_POST_w_index)
 # Calculate VIF
-vif_results <- edu_POST_w_index::vif(model)
+vif_results <- car::vif(model_all)
+
+print("VIF results for post data:")
 print(vif_results)
 
+# Fit a regression model on pre data
+edu_PRE_w_index <-data.frame(read_excel("/home/shivs/r-analysis-pmagy/pre_pmagy_obj3_edu.xlsx",range = "A1:K487"))
+colnames(edu_PRE_w_index) <- paste('PRE', colnames(edu_PRE_w_index), sep = '_')
+
+model_all <- lm(PRE_EDU_INDEX ~ ., data = edu_PRE_w_index)
+# Calculate VIF
+vif_results <- car::vif(model_all)
+print("VIF results for pre data:")
+print(vif_results)
 
 ### EDU Section tests COMPLETE
 print("EDU Section tests COMPLETE")
+
 
