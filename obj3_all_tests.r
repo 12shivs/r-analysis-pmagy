a2e2649c647ca7d180b01f431e929bf65ea5ddc4
12shivs <12shivendra.singh@gmail.com>
2026-01-08 21:10:27 +0530

    prettify

--- obj3_all_tests.r
+++ obj3_all_tests.r
@@ -13,25 +13,25 @@ post_pmagy_file_path <- "/home/shivs/r-analysis-pmagy/post_pmagy.xlsx"
 ### EDU Section tests:
 
 print("EDU Section tests: ")
-#POST PMAGY edu
-edu_POST <-data.frame(read_excel(post_pmagy_file_path,sheet = "Sheet1",range = "AE1:AN487")) #edu_POST
-colnames(edu_POST) <- paste('POST', colnames(edu_POST), sep = '_')
-#PRE PMAGY edu
-edu_PRE <-data.frame(read_excel("/home/shivs/r-analysis-pmagy/pre_pmagy_obj3_edu.xlsx",range = "A1:J487")) #edu_PRE
-colnames(edu_PRE) <- paste('PRE', colnames(edu_PRE), sep = '_')
+# POST PMAGY edu
+edu_POST <- data.frame(read_excel(post_pmagy_file_path, sheet = "Sheet1", range = "AE1:AN487")) # edu_POST
+colnames(edu_POST) <- paste("POST", colnames(edu_POST), sep = "_")
+# PRE PMAGY edu
+edu_PRE <- data.frame(read_excel("/home/shivs/r-analysis-pmagy/pre_pmagy_obj3_edu.xlsx", range = "A1:J487")) # edu_PRE
+colnames(edu_PRE) <- paste("PRE", colnames(edu_PRE), sep = "_")
 
 
-#calculate Cronbach's Alpha:
+# calculate Cronbach's Alpha:
 print(cronbach.alpha(edu_POST))
 print(cronbach.alpha(edu_PRE))
 #
 
-#Variance Inflation Factor (VIF):
+# Variance Inflation Factor (VIF):
 library(car)
 library(caTools)
 # Fit a regression model on post data
-edu_POST_w_index <-data.frame(read_excel(post_pmagy_file_path,sheet = "Sheet1",range = "AE1:AO487"))
-colnames(edu_POST_w_index) <- paste('POST', colnames(edu_POST_w_index), sep = '_')
+edu_POST_w_index <- data.frame(read_excel(post_pmagy_file_path, sheet = "Sheet1", range = "AE1:AO487"))
+colnames(edu_POST_w_index) <- paste("POST", colnames(edu_POST_w_index), sep = "_")
 model_all <- lm(POST_EDU_INDEX ~ ., data = edu_POST_w_index)
 # Calculate VIF
 vif_results <- car::vif(model_all)
@@ -40,15 +40,15 @@ print("VIF results for post data:")
 print(vif_results)
 
 # Fit a regression model on pre data
-edu_PRE_w_index <-data.frame(read_excel("/home/shivs/r-analysis-pmagy/pre_pmagy_obj3_edu.xlsx",range = "A1:K487"))
-colnames(edu_PRE_w_index) <- paste('PRE', colnames(edu_PRE_w_index), sep = '_')
+edu_PRE_w_index <- data.frame(read_excel("/home/shivs/r-analysis-pmagy/pre_pmagy_obj3_edu.xlsx", range = "A1:K487"))
+colnames(edu_PRE_w_index) <- paste("PRE", colnames(edu_PRE_w_index), sep = "_")
 
 model_all <- lm(PRE_EDU_INDEX ~ ., data = edu_PRE_w_index)
 # Calculate VIF
 vif_results <- car::vif(model_all)
 print("VIF results for pre data:")
 print(vif_results)
-#TODO: Visualizing VIF Values from here https://www.geeksforgeeks.org/r-language/vif-function-in-r/
+# TODO: Visualizing VIF Values from here https://www.geeksforgeeks.org/r-language/vif-function-in-r/
 #
 
 # Using theShapiro–Wilk Test :
@@ -58,7 +58,7 @@ print("Shapiro–Wilk Test results for post data:")
 print(shapiro.test(edu_POST_w_index$POST_EDU_INDEX))
 #
 
-# Paired Samples Wilcoxon Test : 
+# Paired Samples Wilcoxon Test :
 
 # combine two data frames horizontally
 edu_pre_post_w_index_hcomb <- cbind(edu_PRE_w_index, edu_POST_w_index)
@@ -69,36 +69,40 @@ paired_wilcoxon_test <- function(before, after) {
 }
 
 # Apply paired Paired Samples Wilcoxon Test to each pair of pre and post columns
-results_wilcoxon_test <- map2(edu_pre_post_w_index_hcomb[grepl("PRE", names(edu_pre_post_w_index_hcomb))],
-                edu_pre_post_w_index_hcomb[grepl("POST", names(edu_pre_post_w_index_hcomb))],
-                paired_wilcoxon_test)
+results_wilcoxon_test <- map2(
+  edu_pre_post_w_index_hcomb[grepl("PRE", names(edu_pre_post_w_index_hcomb))],
+  edu_pre_post_w_index_hcomb[grepl("POST", names(edu_pre_post_w_index_hcomb))],
+  paired_wilcoxon_test
+)
 
 
 # # Define a function to perform paired Paired Samples Wilcoxon Z Test between pairs of columns
 paired_wilcoxon_Z_VALS <- function(before, after) {
-  wilcoxonZ(before, after, paired=TRUE)
+  wilcoxonZ(before, after, paired = TRUE)
 }
 
 # Apply paired Paired Samples Wilcoxon Z Test to each pair of pre and post columns
-results_wilcoxon_Z_VALS <- map2(edu_pre_post_w_index_hcomb[grepl("PRE", names(edu_pre_post_w_index_hcomb))],
-                edu_pre_post_w_index_hcomb[grepl("POST", names(edu_pre_post_w_index_hcomb))],
-                paired_wilcoxon_Z_VALS)
+results_wilcoxon_Z_VALS <- map2(
+  edu_pre_post_w_index_hcomb[grepl("PRE", names(edu_pre_post_w_index_hcomb))],
+  edu_pre_post_w_index_hcomb[grepl("POST", names(edu_pre_post_w_index_hcomb))],
+  paired_wilcoxon_Z_VALS
+)
 
 # # Print the results
 print("Print the results for column/question wise Paired Samples Wilcoxon Test")
 
-  for (i in 1:length(results_wilcoxon_test)) {
-    Z = results_wilcoxon_Z_VALS[[i]]
-      names(Z) = "Z"
-      print(Z)
+for (i in 1:length(results_wilcoxon_test)) {
+  Z <- results_wilcoxon_Z_VALS[[i]]
+  names(Z) <- "Z"
+  print(Z)
 
-  tN = length(results_wilcoxon_test)
-  names(tN) = "tN"
+  tN <- length(results_wilcoxon_test)
+  names(tN) <- "tN"
 
-    r = abs(Z)/sqrt(tN)
-  names(r) = "r"
+  r <- abs(Z) / sqrt(tN)
+  names(r) <- "r"
 
-      print(r)
+  print(r)
 
 
   print(results_wilcoxon_test[[i]])
@@ -113,26 +117,26 @@ print("EDU Section tests COMPLETE")
 ### HN Section tests:
 
 print("HN Section tests: ")
-#POST PMAGY hn
-hn_POST <-data.frame(read_excel(post_pmagy_file_path,sheet = "Sheet1",range = "AP1:AX487")) 
-colnames(hn_POST) <- paste('POST', colnames(hn_POST), sep = '_')
-#PRE PMAGY hn
-hn_PRE <-data.frame(read_excel("/home/shivs/r-analysis-pmagy/pre_pmagy_obj3_hn.xlsx",range = "A1:I487")) 
-colnames(hn_PRE) <- paste('PRE', colnames(hn_PRE), sep = '_')
+# POST PMAGY hn
+hn_POST <- data.frame(read_excel(post_pmagy_file_path, sheet = "Sheet1", range = "AP1:AX487"))
+colnames(hn_POST) <- paste("POST", colnames(hn_POST), sep = "_")
+# PRE PMAGY hn
+hn_PRE <- data.frame(read_excel("/home/shivs/r-analysis-pmagy/pre_pmagy_obj3_hn.xlsx", range = "A1:I487"))
+colnames(hn_PRE) <- paste("PRE", colnames(hn_PRE), sep = "_")
 
 
-#calculate Cronbach's Alpha:
+# calculate Cronbach's Alpha:
 print(cronbach.alpha(hn_POST))
 print(cronbach.alpha(hn_PRE))
 #
 
-#Variance Inflation Factor (VIF):
+# Variance Inflation Factor (VIF):
 library(car)
 library(caTools)
 
 # Fit a regression model on post data
-hn_POST_w_index <-data.frame(read_excel(post_pmagy_file_path,sheet = "Sheet1",range = "AP1:AY487"))
-colnames(hn_POST_w_index) <- paste('POST', colnames(hn_POST_w_index), sep = '_')
+hn_POST_w_index <- data.frame(read_excel(post_pmagy_file_path, sheet = "Sheet1", range = "AP1:AY487"))
+colnames(hn_POST_w_index) <- paste("POST", colnames(hn_POST_w_index), sep = "_")
 
 model_all <- lm(POST_HN_INDEX ~ ., data = hn_POST_w_index)
 # Calculate VIF
@@ -142,8 +146,8 @@ print("VIF results for post data:")
 print(vif_results)
 
 # Fit a regression model on pre data
-hn_PRE_w_index <-data.frame(read_excel("/home/shivs/r-analysis-pmagy/pre_pmagy_obj3_hn.xlsx",range = "A1:J487"))
-colnames(hn_PRE_w_index) <- paste('PRE', colnames(hn_PRE_w_index), sep = '_')
+hn_PRE_w_index <- data.frame(read_excel("/home/shivs/r-analysis-pmagy/pre_pmagy_obj3_hn.xlsx", range = "A1:J487"))
+colnames(hn_PRE_w_index) <- paste("PRE", colnames(hn_PRE_w_index), sep = "_")
 
 model_all <- lm(PRE_HN_INDEX ~ ., data = hn_PRE_w_index)
 
@@ -151,7 +155,7 @@ model_all <- lm(PRE_HN_INDEX ~ ., data = hn_PRE_w_index)
 vif_results <- car::vif(model_all)
 print("VIF results for pre data:")
 print(vif_results)
-#TODO: Visualizing VIF Values from here https://www.geeksforgeeks.org/r-language/vif-function-in-r/
+# TODO: Visualizing VIF Values from here https://www.geeksforgeeks.org/r-language/vif-function-in-r/
 #
 
 # Using theShapiro–Wilk Test :
@@ -161,7 +165,7 @@ print("Shapiro–Wilk Test results for post data:")
 print(shapiro.test(hn_POST_w_index$POST_HN_INDEX))
 #
 
-# Paired Samples Wilcoxon Test : 
+# Paired Samples Wilcoxon Test :
 
 # combine two data frames horizontally
 hn_pre_post_w_index_hcomb <- cbind(hn_PRE_w_index, hn_POST_w_index)
@@ -171,37 +175,41 @@ paired_wilcoxon_test <- function(before, after) {
   wilcox.test(before, after, paired = TRUE)
 }
 
-    # # Apply paired Paired Samples Wilcoxon Test to each pair of pre and post columns
-    results_wilcoxon_test <- map2(hn_pre_post_w_index_hcomb[grepl("PRE", names(hn_pre_post_w_index_hcomb))],
-                    hn_pre_post_w_index_hcomb[grepl("POST", names(hn_pre_post_w_index_hcomb))],
-                    paired_wilcoxon_test)
+# # Apply paired Paired Samples Wilcoxon Test to each pair of pre and post columns
+results_wilcoxon_test <- map2(
+  hn_pre_post_w_index_hcomb[grepl("PRE", names(hn_pre_post_w_index_hcomb))],
+  hn_pre_post_w_index_hcomb[grepl("POST", names(hn_pre_post_w_index_hcomb))],
+  paired_wilcoxon_test
+)
 
 
 # # Define a function to perform paired Paired Samples Wilcoxon Z Test between pairs of columns
 paired_wilcoxon_Z_VALS <- function(before, after) {
-  wilcoxonZ(before, after, paired=TRUE)
+  wilcoxonZ(before, after, paired = TRUE)
 }
 
 # Apply paired Paired Samples Wilcoxon Z Test to each pair of pre and post columns
-results_wilcoxon_Z_VALS <- map2(hn_pre_post_w_index_hcomb[grepl("PRE", names(hn_pre_post_w_index_hcomb))],
-                hn_pre_post_w_index_hcomb[grepl("POST", names(hn_pre_post_w_index_hcomb))],
-                paired_wilcoxon_Z_VALS)
+results_wilcoxon_Z_VALS <- map2(
+  hn_pre_post_w_index_hcomb[grepl("PRE", names(hn_pre_post_w_index_hcomb))],
+  hn_pre_post_w_index_hcomb[grepl("POST", names(hn_pre_post_w_index_hcomb))],
+  paired_wilcoxon_Z_VALS
+)
 
 # # Print the results
 print("Print the results for column/question wise Paired Samples Wilcoxon Test")
 
-  for (i in 1:length(results_wilcoxon_test)) {
-    Z = results_wilcoxon_Z_VALS[[i]]
-      names(Z) = "Z"
-      print(Z)
+for (i in 1:length(results_wilcoxon_test)) {
+  Z <- results_wilcoxon_Z_VALS[[i]]
+  names(Z) <- "Z"
+  print(Z)
 
-  tN = length(results_wilcoxon_test)
-  names(tN) = "tN"
+  tN <- length(results_wilcoxon_test)
+  names(tN) <- "tN"
 
-    r = abs(Z)/sqrt(tN)
-  names(r) = "r"
+  r <- abs(Z) / sqrt(tN)
+  names(r) <- "r"
 
-      print(r)
+  print(r)
 
 
   print(results_wilcoxon_test[[i]])
--- obj4_all_tests.r
+++ obj4_all_tests.r
@@ -13,25 +13,25 @@ post_pmagy_file_path <- "/home/shivs/r-analysis-pmagy/post_pmagy.xlsx"
 ### lisd Section tests:
 
 print("lisd Section tests: ")
-#POST PMAGY lisd
-lisd_POST <-data.frame(read_excel(post_pmagy_file_path,sheet = "Sheet1",range = "CJ1:CN487")) #lisd_POST
-colnames(lisd_POST) <- paste('POST', colnames(lisd_POST), sep = '_')
-#PRE PMAGY lisd
-lisd_PRE <-data.frame(read_excel("/home/shivs/r-analysis-pmagy/pre_pmagy_obj4_lisd.xlsx",range = "A1:E487")) #lisd_PRE
-colnames(lisd_PRE) <- paste('PRE', colnames(lisd_PRE), sep = '_')
+# POST PMAGY lisd
+lisd_POST <- data.frame(read_excel(post_pmagy_file_path, sheet = "Sheet1", range = "CJ1:CN487")) # lisd_POST
+colnames(lisd_POST) <- paste("POST", colnames(lisd_POST), sep = "_")
+# PRE PMAGY lisd
+lisd_PRE <- data.frame(read_excel("/home/shivs/r-analysis-pmagy/pre_pmagy_obj4_lisd.xlsx", range = "A1:E487")) # lisd_PRE
+colnames(lisd_PRE) <- paste("PRE", colnames(lisd_PRE), sep = "_")
 
 
-#calculate Cronbach's Alpha:
+# calculate Cronbach's Alpha:
 print(cronbach.alpha(lisd_POST))
 print(cronbach.alpha(lisd_PRE))
 #
 
-#Variance Inflation Factor (VIF):
+# Variance Inflation Factor (VIF):
 library(car)
 library(caTools)
 # Fit a regression model on post data
-lisd_POST_w_index <-data.frame(read_excel(post_pmagy_file_path,sheet = "Sheet1",range = "CJ1:CO487"))
-colnames(lisd_POST_w_index) <- paste('POST', colnames(lisd_POST_w_index), sep = '_')
+lisd_POST_w_index <- data.frame(read_excel(post_pmagy_file_path, sheet = "Sheet1", range = "CJ1:CO487"))
+colnames(lisd_POST_w_index) <- paste("POST", colnames(lisd_POST_w_index), sep = "_")
 model_all <- lm(POST_LISD_INDEX ~ ., data = lisd_POST_w_index)
 # Calculate VIF
 vif_results <- car::vif(model_all)
@@ -40,14 +40,14 @@ print("VIF results for post data:")
 print(vif_results)
 
 # Fit a regression model on pre data
-lisd_PRE_w_index <-data.frame(read_excel("/home/shivs/r-analysis-pmagy/pre_pmagy_obj4_lisd.xlsx",range = "A1:F487"))
-colnames(lisd_PRE_w_index) <- paste('PRE', colnames(lisd_PRE_w_index), sep = '_')
+lisd_PRE_w_index <- data.frame(read_excel("/home/shivs/r-analysis-pmagy/pre_pmagy_obj4_lisd.xlsx", range = "A1:F487"))
+colnames(lisd_PRE_w_index) <- paste("PRE", colnames(lisd_PRE_w_index), sep = "_")
 model_all <- lm(PRE_LISD_INDEX ~ ., data = lisd_PRE_w_index)
 # Calculate VIF
 vif_results <- car::vif(model_all)
 print("VIF results for pre data:")
 print(vif_results)
-#TODO: Visualizing VIF Values from here https://www.geeksforgeeks.org/r-language/vif-function-in-r/
+# TODO: Visualizing VIF Values from here https://www.geeksforgeeks.org/r-language/vif-function-in-r/
 #
 
 # Using theShapiro–Wilk Test :
@@ -57,7 +57,7 @@ print("Shapiro–Wilk Test results for post data:")
 print(shapiro.test(lisd_POST_w_index$POST_LISD_INDEX))
 #
 
-# Paired Samples Wilcoxon Test : 
+# Paired Samples Wilcoxon Test :
 
 # combine two data frames horizontally
 lisd_pre_post_w_index_hcomb <- cbind(lisd_PRE_w_index, lisd_POST_w_index)
@@ -68,35 +68,39 @@ paired_wilcoxon_test <- function(before, after) {
 }
 
 # # Apply paired Paired Samples Wilcoxon Test to each pair of pre and post columns
-results_wilcoxon_test <- map2(lisd_pre_post_w_index_hcomb[grepl("PRE", names(lisd_pre_post_w_index_hcomb))],
-                lisd_pre_post_w_index_hcomb[grepl("POST", names(lisd_pre_post_w_index_hcomb))],
-                paired_wilcoxon_test)
+results_wilcoxon_test <- map2(
+  lisd_pre_post_w_index_hcomb[grepl("PRE", names(lisd_pre_post_w_index_hcomb))],
+  lisd_pre_post_w_index_hcomb[grepl("POST", names(lisd_pre_post_w_index_hcomb))],
+  paired_wilcoxon_test
+)
 
 # # Define a function to perform paired Paired Samples Wilcoxon Z Test between pairs of columns
 paired_wilcoxon_Z_VALS <- function(before, after) {
-  wilcoxonZ(before, after, paired=TRUE)
+  wilcoxonZ(before, after, paired = TRUE)
 }
 
 # Apply paired Paired Samples Wilcoxon Z Test to each pair of pre and post columns
-results_wilcoxon_Z_VALS <- map2(lisd_pre_post_w_index_hcomb[grepl("PRE", names(lisd_pre_post_w_index_hcomb))],
-                lisd_pre_post_w_index_hcomb[grepl("POST", names(lisd_pre_post_w_index_hcomb))],
-                paired_wilcoxon_Z_VALS)
+results_wilcoxon_Z_VALS <- map2(
+  lisd_pre_post_w_index_hcomb[grepl("PRE", names(lisd_pre_post_w_index_hcomb))],
+  lisd_pre_post_w_index_hcomb[grepl("POST", names(lisd_pre_post_w_index_hcomb))],
+  paired_wilcoxon_Z_VALS
+)
 
 # # Print the results
 print("Print the results for column/question wise Paired Samples Wilcoxon Test")
 
-  for (i in 1:length(results_wilcoxon_test)) {
-    Z = results_wilcoxon_Z_VALS[[i]]
-      names(Z) = "Z"
-      print(Z)
+for (i in 1:length(results_wilcoxon_test)) {
+  Z <- results_wilcoxon_Z_VALS[[i]]
+  names(Z) <- "Z"
+  print(Z)
 
-  tN = length(results_wilcoxon_test)
-  names(tN) = "tN"
+  tN <- length(results_wilcoxon_test)
+  names(tN) <- "tN"
 
-    r = abs(Z)/sqrt(tN)
-  names(r) = "r"
+  r <- abs(Z) / sqrt(tN)
+  names(r) <- "r"
 
-      print(r)
+  print(r)
 
 
   print(results_wilcoxon_test[[i]])
@@ -106,4 +110,3 @@ print("Print the results for column/question wise Paired Samples Wilcoxon Test")
 
 ### lisd Section tests COMPLETE
 print("lisd Section tests COMPLETE")
-
