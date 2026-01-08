622731ac6b1d9f8a3b8f90e6fcf9b30a28edf867
12shivs <12shivendra.singh@gmail.com>
2026-01-08 18:42:51 +0530

    charts ready

--- chart_gen.r
+++ chart_gen.r
@@ -145,3 +145,135 @@ library(caTools)
 # ylab = "PRE_LISD_INDEX",
 # col = "steelblue",
 # border = "black")
+
+# vec<-dplyr::pull(pmagy_POST, "DI.1..GENDER")
+# gen_count <- c(
+#     sum(vec == "Male")  
+
+# ,
+
+#     sum(vec == "Female")  
+# )
+# labels <- c("Male", "Female")
+
+# piepercent<- round(100 * gen_count / sum(gen_count), 1)
+
+# pie(gen_count, labels = piepercent,
+#     main = "...", col = rainbow(length(gen_count)))
+#     legend("topright", c("Male", "Female"),
+#                     cex = 0.5, fill = rainbow(length(gen_count)))
+
+
+# age,, ed qual, primary occ, annual inc, di8, di10, di11 pie charts
+
+# table(pmagy_POST$DI.2...AGE)
+# # 60 18-30 31-40 41-50 51-60 
+# # 8   204   148    61    65 
+
+# geeks <- c(8   ,204   ,148    ,61   ,65 )
+# labels <- c("60","18-30","31-40","41-50", "51-60")
+
+# piepercent<- round(100 * geeks / sum(geeks), 1)
+
+# pie(geeks, labels = piepercent,
+#     main = "...", col = rainbow(length(geeks)))
+#     legend("topright", labels,
+#                     cex = 0.5, fill = rainbow(length(geeks)))
+
+
+# table(pmagy_POST$DI.4...EDUCATION.QUALIFICATION)
+# # Diploma Graduate and Above   Higher Secondary         Illiterate          Secondary 
+# #                 17                191                148                 61                 69 
+
+# geeks <- c(17         ,       191      ,          148         ,        61        ,         69 )
+# labels <- c("Diploma Graduate and Above"  , "Higher Secondary"   ,     "Illiterate"       ,   "Secondary" )
+
+# piepercent<- round(100 * geeks / sum(geeks), 1)
+
+# pie(geeks, labels = piepercent,
+#     main = "...", col = rainbow(length(geeks)))
+# legend("topright", labels,
+#        cex = 0.5, fill = rainbow(length(geeks)))
+
+
+
+# table(pmagy_POST$DI.5..PRIMARY.OCCUPATION)
+# #  agriculture                  labour        self employed/ business          unemployed/ not working 
+# #        299                     141                      34                      12 
+
+# geeks <- c( 299            ,         141             ,         34          ,            12 )
+# labels <- c("agriculture"         ,         "labour"    ,    "self employed/ business"      ,    "unemployed/ not working")
+
+# piepercent<- round(100 * geeks / sum(geeks), 1)
+
+# pie(geeks, labels = piepercent,
+#     main = "...", col = rainbow(length(geeks)))
+#     legend("topright", labels,
+#                     cex = 0.5, fill = rainbow(length(geeks)))
+# # 1116 586
+
+
+
+# table(pmagy_POST$DI.6..ANNUAL.INCOME.OF.THE.HOUSEHOLD)
+# #  25,000 Rupees        > 75,000   25,000-50,000   50,000-75,000       no income 
+# #              10             258              77             137               4 
+
+# geeks <- c(  10      ,       258        ,      77     ,        137   ,            4 )
+# labels <- c("25,000 Rupees"     ,   "> 75,000"  , "25,000-50,000"  , "50,000-75,000"    ,   "No income")
+
+# piepercent<- round(100 * geeks / sum(geeks), 1)
+
+# pie(geeks, labels = piepercent,
+#     main = "...", col = rainbow(length(geeks)))
+#     legend("topright",  c("25,000 Rupees"     ,   "> 75,000"  , "25,000-50,000"  , "50,000-75,000"    ,   "No income"),
+#                     cex = 0.5, fill = rainbow(length(geeks)))
+# # 1116 586
+
+
+# table(pmagy_POST$DI.8...If.yes..source.of.information.about.PMAGY.)
+# # family/friends/fellow villagers                  Gram Panchayat                       newspaper            Radio and Television 
+# #                       95                             331                              17                              43 
+
+# geeks <- c(  95            ,                 331        ,                      17           ,                   43 )
+# labels <- c("family/friends/fellow villagers"                  ,"Gram Panchayat"              ,         "newspaper"        ,    "Radio and Television")
+
+# piepercent<- round(100 * geeks / sum(geeks), 1)
+
+# pie(geeks, labels = piepercent,
+#     main = "...", col = rainbow(length(geeks)))
+#     legend("topright", labels,
+#                     cex = 0.5, fill = rainbow(length(geeks)))
+# # 1116 586
+
+
+
+# table(pmagy_POST$DI.10...To.what.extent.do.you.feel.more.included.in.the.village.s.decision.making.processes.after.the.implementation.of.PMAGY.)
+# # completely included     mostly included   Slightly included   somewhat included 
+# #                 364                  61                   8                  53 
+
+# geeks <- c(364          ,        61          ,         8          ,        53 )
+# labels <- c("completely included",     "mostly included",   "Slightly included",   "somewhat included")
+
+# piepercent<- round(100 * geeks / sum(geeks), 1)
+
+# pie(geeks, labels = piepercent,
+#     main = "...", col = rainbow(length(geeks)))
+#     legend("topright", labels,
+#                     cex = 0.5, fill = rainbow(length(geeks)))
+# # 1116 586
+
+
+table(pmagy_POST$DI.11...What.change.have.you.noticed.in.the.treatment.of.Scheduled.Castes.in.your.village.since.PMAGY.)
+# dont know/ no opinion     major improvement             no change      some improvement 
+#                    28                   400                    11                    47 
+
+geeks <- c( 28,                   400,                    11,                    47 )
+labels <- c("dont know/ no opinion",     "major improvement",             "no change",      "some improvement" )
+
+piepercent<- round(100 * geeks / sum(geeks), 1)
+
+pie(geeks, labels = piepercent,
+    main = "...", col = rainbow(length(geeks)))
+    legend("topright", labels,
+                    cex = 0.5, fill = rainbow(length(geeks)))
+# 1116 586
